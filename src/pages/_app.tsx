8880f1d5d18a0d8f9a62a795aa8572119434e243
Zahara (she/her) <117520023+zahara-aviv@users.noreply.github.com>
2023-02-21 10:48:23 -0500

    Revert "Update gitignore"

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
