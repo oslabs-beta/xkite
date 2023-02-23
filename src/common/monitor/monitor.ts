f213947e40c7b7b100e4f05c6e314ee0bfd5df58
khafizka <irinakhafi@gmail.com>
2023-02-23 14:38:54 -0500

    adds an ability to host display page on localhost 6662

--- src/common/monitor/monitor.ts
+++ src/common/monitor/monitor.ts
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 
 export default class Monitor {
   public static initiate() {
-    const child = spawn('open', ['http://localhost:3000/display']);
+    const child = spawn('open', ['http://localhost:6662/display']);
     child.on('error', (err: any) => {
       console.error('Failed to open localhost:', err);
     });
