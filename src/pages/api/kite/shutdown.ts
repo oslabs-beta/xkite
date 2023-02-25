fcaf912dc22ca9f9e7d4e807830c5d42d2d5c235
khafizka <irinakhafi@gmail.com>
2023-02-25 16:22:53 -0500

    adds port flexibilty and ability to close pages at shutdown

--- src/pages/api/kite/shutdown.ts
+++ src/pages/api/kite/shutdown.ts
@@ -1,6 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Kite from '@/common/kite';
+import { spawn } from 'child_process';
+
+const urls: string[] = [
+  `http://localhost:${process.env.PORT1 || 6661}/`,
+  `http://localhost:${process.env.PORT1 || 6661}/setup`,
+  `http://localhost:${process.env.PORT2 || 6662}/display`
+];
+
 
 type Result = {
   result?: string;
@@ -13,6 +20,15 @@ export default async function handler(
 ) {
   if (req.method === 'DELETE') {
     const kite = Kite.shutdown();
+    for (const url of urls) {
+      if (process.platform === 'darwin') {
+        // For macOS
+        spawn('osascript', ['-e', `tell application "Google Chrome" to close (tabs of window 1 whose URL is "${url}")`]);
+      } else {
+        // For other platforms
+        spawn('taskkill', ['/F', '/IM', 'chrome.exe']);
+      }
+    }
     res.status(200).json({ result: 'success' });
   } else {
     res.status(405).send({ error: 'Method Not Allowed' });
